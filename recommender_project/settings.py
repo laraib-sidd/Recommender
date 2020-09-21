3eb7db2982a37b03beb2a39ee6ec5faa1f7c585e
laraib-sidd <laraib.sidd7>
2020-09-21 23:10:48 +0530

    update

--- recommender_project/settings.py
+++ recommender_project/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+LOGOUT_REDIRECT_URL = '/'
