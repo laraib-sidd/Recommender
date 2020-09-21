4989428cfdc5bca72337cdb09724758e809b7450
laraib-sidd <laraib.sidd7>
2020-09-21 17:52:58 +0530

    update database

--- recommender_project/settings.py
+++ recommender_project/settings.py
@@ -75,13 +75,18 @@ WSGI_APPLICATION = "recommender_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
